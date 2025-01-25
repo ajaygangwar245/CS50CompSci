aa37bbad3734c9b62c4d81235324ea7800fdcad8
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-25 22:34:11 +0530

    added practice problem

---
+++ week6/compare2.py
@@ -0,0 +1,11 @@
+#compare two strings
+
+str1 = input("Text 1: ").lower()
+str2 = input("Text 2: ").lower()
+
+if str1 == str2:
+    print("Both strings are same")
+else:
+    print("Strings are not same")
+
+
