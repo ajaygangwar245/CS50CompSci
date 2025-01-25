cfef16c2b69abb936e25195f72a78dbaa8e860e1
Ajay Gangwar <ajaygangwar24598@gmail.com>
2025-01-25 22:33:24 +0530

    added practice problem

---
+++ week6/add.py
@@ -0,0 +1,11 @@
+#add two ints without get_int from cs50
+
+n1 = input("n1: ")
+n2 = input("n2: ")
+
+print(int(n1) + int(n2))
+
+# n1 = int(input("n1: "))
+# n2 = int(input("n2: "))
+
+# print(n1 + n2)
